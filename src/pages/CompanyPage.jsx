bf4e0ef095475fea36b2ae49444a4aac13ab32d2
Katia <kate.liashevska@gmail.com>
2023-07-28 17:44:18 +0200

    lab done

--- src/pages/CompanyPage.jsx
+++ src/pages/CompanyPage.jsx
@@ -1,9 +1,30 @@
-function CompanyPage() {
-  return (
-    <div>
-      <h1>CompanyPage</h1>
-    </div>
-  );
+import { useParams, Link } from "react-router-dom"
+
+function CompanyPage({ companies }) {
+    const { companySlug } = useParams()
+    const companyMatch = companies.find(company => company.slug === companySlug)
+
+    return (
+        <div>
+            <h1>Company Profile</h1>
+            <div className="company-page-list">
+                <a href={`https://${companyMatch.website}`} className="company-name">
+                    {companyMatch.name}
+                </a>
+                <img src={companyMatch.logo} alt={`${companyMatch.name} logo`} className="company-image" />
+                <p className="company-description">{companyMatch.description}</p>
+            </div>
+
+            <div className="tech-stack-list">
+                {companyMatch.techStack.map(tech => (
+                    <Link to={`/tech/${tech.slug}`} key={tech.slug} className="tech-card">
+                        <img src={tech.image} alt={`${tech.name} logo`} className="tech-image" />
+                        {tech.name}
+                    </Link>
+                ))}
+            </div>
+        </div>
+    )
 }
 
-export default CompanyPage;
+export default CompanyPage
--- src/pages/TechnologyPage.jsx
+++ src/pages/TechnologyPage.jsx
@@ -1,9 +1,19 @@
-function TechnologyPage() {
-  return (
-    <div>
-      <h1>TechnologyPage</h1>
-    </div>
-  );
+import { useParams, Link } from "react-router-dom"
+
+function TechnologyPage({ technologies }) {
+    const { slug } = useParams()
+    const techMatch = technologies.find(tech => tech.slug === slug)
+
+    return (
+        <div>
+            <h1>Technology Details</h1>
+            <div className="tech-page-list">
+                <h2>{techMatch.name}</h2>
+                <img src={techMatch.image} alt={`${techMatch.name} logo`} className="tech-image" />
+                <p className="tech-description">{techMatch.description}</p>
+            </div>
+        </div>
+    )
 }
 
-export default TechnologyPage;
+export default TechnologyPage
