7360b37444e0bef1cf9e13b2a0766421be2bda55
FaresQaddoura29 <faresqaddoura@gmail.com>
2023-11-03 17:04:06 +0000

    done

--- src/pages/CompanyPage.jsx
+++ src/pages/CompanyPage.jsx
@@ -1,7 +1,33 @@
-function CompanyPage() {
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { useParams, Link } from "react-router-dom";
+
+function CompanyPage(props) {
+  const {companySlug} = useParams()
+  const {companyArr, techArr} = props;
+ 
+  const foundCompany = companyArr.find(company=>company.slug === companySlug)
+
+  const {name, description, website, logo} = foundCompany;
+  const techMap = foundCompany.techStack.map((tech, index)=>{
+    const div = 
+    <div className="company-tech-card">
+      <img src={tech.image}/>
+      <p>{tech.name}</p>
+    </div>
+    return <Link key={index} to={`/tech/${tech.slug}`}>{div}</Link>
+  })
+
   return (
-    <div>
-      <h1>CompanyPage</h1>
+
+    <div className="company-info">
+      <img src={logo} />
+      <h1>{name}</h1>
+      <p>{description}</p>
+      <h3>Tech Stack</h3>
+      <div id="company-techstack-div">
+        {techMap}
+      </div>
     </div>
   );
 }
--- src/pages/TechnologyPage.jsx
+++ src/pages/TechnologyPage.jsx
@@ -1,7 +1,15 @@
-function TechnologyPage() {
+import { useParams } from "react-router-dom";
+ 
+function TechnologyPage(prop) {
+  const {slug} = useParams();
+  const foundTech = prop.techArr.find(tech=>tech.slug === slug)
+  const {name, image, description} = foundTech
   return (
-    <div>
-      <h1>TechnologyPage</h1>
+    <div className="company-info">
+      <img src={image}/>
+      <h1>{name}</h1>
+      <p>{description}</p>
+      <button to="">Back</button>
     </div>
   );
 }
