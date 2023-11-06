09fff1e0c789dde54abdb7b93102597f956edf12
Guilherme Silva <guilhermemas2004@gmail.com>
2023-11-06 08:47:42 +0000

    done

--- src/pages/TechnologyPage.jsx
+++ src/pages/TechnologyPage.jsx
@@ -1,9 +1,21 @@
-function TechnologyPage() {
-  return (
-    <div>
-      <h1>TechnologyPage</h1>
-    </div>
-  );
-}
+import { useParams, useNavigate, useSearchParams, Link } from "react-router-dom"
 
+function TechnologyPage({ technologies }) {
+    const { slug } = useParams()
+    const techMatch = technologies.find(tech => tech.slug === slug)
+    const [searchParams, setSearchParams] = useSearchParams()
+    const previousCompany = searchParams.get("company")
+
+    return (
+        <div>
+            <h1>Technology Details</h1>
+            <div className="tech-page-list">
+                <h2>{techMatch.name}</h2>
+                <img src={techMatch.image} alt={`${techMatch.name} logo`} className="tech-image" />
+                <p className="tech-description">{techMatch.description}</p>
+                <Link to={`/company/${previousCompany}`}>Back</Link>
+            </div>
+        </div>
+    )
+}
 export default TechnologyPage;
