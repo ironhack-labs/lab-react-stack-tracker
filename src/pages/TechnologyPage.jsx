368113a321c0eab576523f6a2c7b83e858906aef
Filipe Dias <dias.filipe2000gmail.com>
2023-11-06 08:50:34 +0000

    done

--- src/pages/TechnologyPage.jsx
+++ src/pages/TechnologyPage.jsx
@@ -1,7 +1,36 @@
-function TechnologyPage() {
+import { useParams, useSearchParams, Link } from "react-router-dom";
+
+function TechnologyPage(props) {
+  const { slug } = useParams();
+  const [searchParams] = useSearchParams();
+  const visitedCompanySlug = searchParams.get("visitedCompanySlug");
+
+  const foundTechnology = props.technologies.find(
+    (technology) => technology.slug === slug
+  );
+
   return (
-    <div>
+     <div>
       <h1>TechnologyPage</h1>
+      <p>Technology Details</p>
+
+      <div className="technology-summary">
+        <div className="technology-logo-wrapper">
+          <img className="logo" src={foundTechnology.image} alt="" />
+        </div>
+        <div>
+          <p>
+            <h1>{foundTechnology.name}</h1>
+            <b>About</b>
+          </p>
+          <p>{foundTechnology.description}</p>
+          <br />
+        </div>
+      </div>
+
+      <Link to={`/company/${visitedCompanySlug}`}>
+        <button>Back</button>
+      </Link>
     </div>
   );
 }
