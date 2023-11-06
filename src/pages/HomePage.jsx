3617bb64e92357fb41a40343ec771eb1677808b1
Caroline Vallim <carolinecvallim@gmail.com>
2023-11-06 06:26:09 +0000

    final update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,25 @@
-function HomePage() {
+import { Link } from "react-router-dom";
+
+
+function HomePage(props) {
+  let {companies} = props;
   return (
-    <div>
-      <h1>HomePage</h1>
+    <div id="home-page">
+      <div>
+        <h1>StackTracker: Discover Tech Stacks Used by Top Companies</h1>
+      </div>
+      <div id="company-grid">
+        {companies.map((company)=>{
+          return (
+            <div id="company-card">
+              <Link key={company.id} to={`/company/${company.slug}`}>
+              {company.name}
+              <img id="logo" src={company.logo} />
+              </Link>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
