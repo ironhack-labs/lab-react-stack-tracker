3b9230561f6741dae52e091ac454d0e809202066
Miguel Mestre <mestremn28@hotmail.com>
2023-11-06 00:46:59 +0000

    5day4week

--- src/pages/TechnologyPage.jsx
+++ src/pages/TechnologyPage.jsx
@@ -1,9 +1,17 @@
+import React from 'react';
+
 function TechnologyPage() {
   return (
     <div>
-      <h1>TechnologyPage</h1>
+      <h1>Technology Details</h1>
+      {/* Add the rest of your technology page content here */}
     </div>
   );
 }
 
 export default TechnologyPage;
+
+
+
+
+
