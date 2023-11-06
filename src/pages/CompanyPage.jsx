4a27e6fd9da9d1c88be85c32bcb91705a3fe4a6c
Daniel Rodrigues <danielbrodrigues25@gmail.com>
2023-11-06 00:15:52 +0000

    done

--- src/pages/CompanyPage.jsx
+++ src/pages/CompanyPage.jsx
@@ -1,7 +1,34 @@
-function CompanyPage() {
+import React from 'react';
+import { useParams, Link } from "react-router-dom";
+
+function CompanyPage({ companies }) {
+  const { companySlug } = useParams();
+  const foundCompany = companies.find(company => company.slug === companySlug);
+
+  if (!foundCompany) {
+    return <div>Company not found.</div>;
+  }
+
   return (
     <div>
-      <h1>CompanyPage</h1>
+      <h1>Company Profile</h1>
+      <div>
+        <h2>{foundCompany.name}</h2>
+        <p><a href={foundCompany.website}>{foundCompany.website}</a></p>
+        <p>{foundCompany.description}</p>
+        <img src={foundCompany.logo} alt={`${foundCompany.name} Logo`} />
+        <h3>Tech Stack:</h3>
+        <ul>
+          {foundCompany.techStack.map(tech => (
+            <li key={tech.slug} className = "tech-item">
+              <Link to={`/tech/${tech.slug}?visitedCompanySlug=${companySlug}`}>
+                <img src={tech.image} alt={`${tech.name} Logo`} /> 
+                {tech.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
