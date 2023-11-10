a004ba893a48a7cc31985be7b65ca897d83952ea
Isabel Robleda <isabel.robleda@gmail.com>
2023-11-10 19:26:32 +0100

    lab done

--- src/pages/CompanyPage.jsx
+++ src/pages/CompanyPage.jsx
@@ -1,7 +1,23 @@
-function CompanyPage() {
+import { useParams } from "react-router-dom";
+import TechnologyPage from "./TechnologyPage";
+
+function CompanyPage(props) {
+  
+  const {companySlug} = useParams()
+
+  const companyDetails = props.companiesArr.find((company) => {
+    return company.slug === companySlug
+  });
+
+  
   return (
     <div>
-      <h1>CompanyPage</h1>
+      <h1>Company: {companyDetails.name}</h1>
+      <h2>Wesite: {companyDetails.website}</h2>
+      <p> {companyDetails.description}</p>
+      <img className="logoDetails"src={companyDetails.logo}/>
+
+      <TechnologyPage techStack={companyDetails.techStack} />
     </div>
   );
 }
--- src/pages/TechnologyPage.jsx
+++ src/pages/TechnologyPage.jsx
@@ -1,8 +1,30 @@
-function TechnologyPage() {
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+function TechnologyPage(props) {
+  
+  const {techSlug} = useParams()
+
+  const techDetails = props.techStack.find((tech) => {
+    return tech.slug === techSlug
+  });
+
+  
   return (
-    <div>
-      <h1>TechnologyPage</h1>
-    </div>
+    <ul className="listTech">
+    {props.techStack.map((techObj) => {
+      return (
+        <>
+          <div className="techStack">
+            <Link to={`/tech/${techObj.slug}`}>
+              <p>{techObj.name} </p>
+              {<img className="techLogo" src={techObj.image} />}
+            </Link>
+          </div>
+        </>
+      );
+    })}
+  </ul>
   );
 }
 
