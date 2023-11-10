7123f4472877fd2b623137bece9f2763f24a6d28
Hugo Manachem <hugomanachem.pro@gmail.com>
2023-11-10 18:11:57 +0100

    Exercices + Bonus done!

--- src/pages/CompanyPage.jsx
+++ src/pages/CompanyPage.jsx
@@ -1,7 +1,37 @@
-function CompanyPage() {
+import { Link, useParams } from "react-router-dom";
+import "../index.css";
+
+function CompanyPage(props) {
+  const { companySlug } = useParams();
+
+  const companyDetails = props.companiesArray.find((company) => {
+    return company.slug == companySlug;
+  });
+
   return (
     <div>
-      <h1>CompanyPage</h1>
+      <h1>Company Profile</h1>
+      <div className="companyprofile-company">
+      <p><strong>{companyDetails.name}</strong></p>
+      <img src={companyDetails.logo} />
+      <p>{companyDetails.website}</p>
+      <p>{companyDetails.description}</p>
+      </div>
+      
+      <div className="companyprofile-techs-grid">
+      {companyDetails.techStack.map((tech) => {
+        return (
+          <div className="companyprofile-techs-card">
+            <Link to={"/tech/" + tech.slug + "?companyslug=" + companySlug}>
+            <p>{tech.name}</p>
+            <img src={tech.image} />
+          </Link>
+          </div>
+          
+        );
+      })}
+      </div>
+      
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,22 @@
-function HomePage() {
+import { Link } from "react-router-dom";
+import "../index.css";
+
+function HomePage(props) {
   return (
     <div>
-      <h1>HomePage</h1>
+      <h1>StackTracker: Discover Tech Stacks Used by Top Companies</h1>
+      <div id="homepage-company-grid">
+        {props.companiesArray.map((company) => {
+          return (
+            <div className="homepage-company-card">
+              <Link to={`/company/${company.slug}`}>
+                <p>{company.name}</p>
+                <img src={company.logo} alt="Logo of the company" />
+              </Link>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
