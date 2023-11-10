b8ec1b6bf7cfce488cafdcce98ffadea0fad90ad
NISHA PATIL <prashantpatil@prashants-air.kpn>
2023-11-10 19:20:33 +0100

    iteration 1-4 solved

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,20 @@
-function HomePage() {
+import companies from "../companies.json"
+import {Link} from "react-router-dom";
+function HomePage(props) {
   return (
     <div>
-      <h1>HomePage</h1>
+      <h1>"StackTracker: Discover Tech Stacks Used by Top Companies"</h1>
+      <ul>
+        {props.companies.map((company) => (
+          <li key={company.id}>
+             <Link to={`/company/${company.slug}`}>
+             {company.name}
+              <img src={company.logo}/></Link>
+          </li>
+
+        ))}
+      </ul>
+      
     </div>
   );
 }
