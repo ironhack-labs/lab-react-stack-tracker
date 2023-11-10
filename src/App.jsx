e2b5d9e451f50780e4a36bc296972586d09ed450
Fran Martinez <pmg.webdev@gmail.com>
2023-11-10 16:48:36 +0100

    it 1, imports json databases in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import "./App.css";
+import companies from "./companies.json"
+import technologies from "./technologies.json"
+import { useState } from "react";
+
+
 
 function App() {
+
+  
+const [companiesToDisplay, setCompaniesToDisplay] = useState(companies);
+const [technologiesToDisplay, setTechnologiesToDisplay] = useState(technologies);
+
   return (
     <div className="App">
       <h1>LAB | React Stack Tracker</h1>
