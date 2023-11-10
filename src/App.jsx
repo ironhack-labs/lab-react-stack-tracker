9eb27f46807c23206ba766bf8d564644c9541518
mayakilbertus <maya.kilbertus@yahoo.de>
2023-11-10 16:45:31 +0100

    Iteration 1 & 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import "./App.css";
+import companies from "./companies.json";
+import technologies from "./technologies.json";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import CompanyPage from "./pages/CompanyPage";
+import TechnologyPage from "./pages/TechnologyPage";
 
 function App() {
+  const [companiesData, setCompaniesData] = useState(companies);
+  const [technologiesData, setTechnologiesData] = useState(technologies);
+
   return (
     <div className="App">
-      <h1>LAB | React Stack Tracker</h1>
+      <HomePage />
     </div>
   );
 }
