a80783bdbf4e2258c8f8a4dcc45e8dfb9793f48f
Fran Martinez <pmg.webdev@gmail.com>
2023-11-11 19:04:07 +0100

    Access URL paramaters and Shows company details

--- src/pages/CompanyPage.jsx
+++ src/pages/CompanyPage.jsx
@@ -1,13 +1,27 @@
 import {useParams} from 'react-router-dom'
+
 function CompanyPage(props) {
-  const {companySlug} = useParams()
-  console.log('hi')
+
+  const {companySlug} = useParams();
+
+  // console.log(props.companiesArr)
+  // console.log(companySlug)
+  const [companyMatch] = props.companiesArr.filter((company)=>{
+    return company.slug === companySlug;
+  })
+  console.log(companyMatch)
+
   return (
     <div>
       <h2>Company Profile</h2>
-      <h3>{companySlug}</h3>
+      <img className='logo' src={companyMatch.logo} alt="" />
+      <h2>{companySlug}</h2>
+      <h3>About</h3>
+      <p>{companyMatch.description}</p>
+      <a href={companyMatch.website}>Visit website</a>
     </div>
   );
+  
 }
 
 export default CompanyPage;
