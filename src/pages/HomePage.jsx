ff20c3b7ec7bd73f0b48b9b0d3deba854c03b2c7
“elisejonkers” <elisejonkers18@hotmail.com>
2023-11-12 15:08:24 +0100

    created routes and list item logos and names in flexbox

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,23 @@
-function HomePage() {
+function HomePage(props) {
   return (
     <div>
-      <h1>HomePage</h1>
+      <h2>StackTracker: Discover Tech Stacks Used by Top Companies</h2>
+
+      {console.log(props.listOfCompanies[0])}
+
+      {props.listOfCompanies.map(function(element){
+        return (
+          <ul>
+            <li key={element.id}>{element.name} <img src={element.logo} /></li>
+          </ul>
+        )
+        //console.log(element)
+      })}
+
+
+    
+
+
     </div>
   );
 }
