f78e88ce21a9a0cbaaae046f7bc608909625e3bb
Hugo Pereira <hugocastroferro@gmail.com>
2023-11-12 18:37:22 +0000

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,12 @@
+import { NavLink } from "react-router-dom";
+import React from 'react';
+
 function Navbar() {
-  return <nav>Navbar</nav>;
+  return (
+    <nav>
+      <h1>StackTracker</h1>
+    </nav>
+  );
 }
 
 export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,22 @@
-function HomePage() {
+import React from 'react';
+import { Link } from "react-router-dom";
+import "./HomePage.css";
+
+function HomePage({ companies }) {
   return (
-    <div>
-      <h1>HomePage</h1>
+    <div className="home-page">
+      <h1>Discover Tech Stacks Used by Top Companies</h1>
+
+      <ul className="company-list">
+        {companies.map((company) => (
+          <li key={company.slug}>
+            <Link to={`/company/${company.slug}`}>
+              <img src={company.logo} alt={`${company.name} Logo`} className="company-logo" />
+              <span className="company-name">{company.name}</span>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
