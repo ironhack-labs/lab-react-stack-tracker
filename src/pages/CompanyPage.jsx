aeff5c23bb11fc55ae1d383c7bd90a62d614124c
Amir Mansour <swasat2009@hotmail.com>
2023-11-12 12:46:13 +0100

    massive headache

--- src/pages/CompanyPage.jsx
+++ src/pages/CompanyPage.jsx
@@ -1,7 +1,21 @@
-function CompanyPage() {
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+
+function CompanyPage(props) {
+  const { companySlug } = useParams();
+
+  const companyDetails = props.companiesArr.find((company) => {
+    return company.slug === companySlug;
+  });
+
   return (
     <div>
-      <h1>CompanyPage</h1>
+      <h1>Company Profile</h1>
+
+      <h1>Company: {companyDetails.name}</h1>
+      <h2>Website: {companyDetails.website}</h2>
+      <section>Description: {companyDetails.description}</section>
+      <img src={companyDetails.logo} alt="" />
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,23 @@
-function HomePage() {
+import { Link } from "react-router-dom";
+
+function HomePage(props) {
+ 
+ //console.log(props.companies); --------> returns undefined???
   return (
     <div>
-      <h1>HomePage</h1>
+      <h1>StackTracker: Discover Tech Stacks Used by Top Companies</h1>
+
+      <ul>
+        {props.companies &&
+          props.companies.map((company) => (
+            <li key={company.id}>
+              <Link to={`/company/${company.slug}`}>
+                <img src={company.logo} alt={company.companyName} />
+                {company.companyName}
+              </Link>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
