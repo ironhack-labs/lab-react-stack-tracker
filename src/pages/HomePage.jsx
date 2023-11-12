e53ddbe77fcd1162ac4a6b7de39bf1fb29b83ae6
mayakilbertus <maya.kilbertus@yahoo.de>
2023-11-12 16:50:34 +0100

    come CSS

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,16 +4,18 @@ function HomePage(props) {
   return (
     <div>
       <h1>StackTracker: Discover Tech Stacks Used by Top Companies</h1>
-      <ul>
+      <div className="container">
         {props.companiesList.map((company, i) => {
           return (
-            <Link key={i} to={`/company/${company.slug}`} className="card">
-              {company.name}
-              <img src={company.logo} className="logo" />
-            </Link>
+            <div className="card">
+              <Link key={i} to={`/company/${company.slug}`}>
+                <p>{company.name}</p>
+                <img src={company.logo} className="logo" />
+              </Link>
+            </div>
           );
         })}
-      </ul>
+      </div>
     </div>
   );
 }
