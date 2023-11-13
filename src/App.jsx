847b2eb7f299cf78d058206401235fe04e951cd6
Zohra <zohra.boukhatem1@gmail.com>
2023-11-13 09:02:33 +0100

    stuck at iteration 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import "./App.css";
+import HomePage from "./pages/HomePage"
+import CompanyPage from "./pages/CompanyPage"
+import TechnologyPage from "./pages/TechnologyPage"
+import companiesJson from "./companies.json";
+import technologiesJson from "./technologies.json";
+import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [companies, setCompanies] = useState(companiesJson);
+  const [technologies, setTechnologies] = useState(technologiesJson);
+
   return (
     <div className="App">
-      <h1>LAB | React Stack Tracker</h1>
+      <Routes>
+        <Route path="/" element={ <HomePage companies = {companies}/>}/>
+        <Route path="/company/:companySlug"element={ <CompanyPage companies = {companies}/>}/>
+        <Route path="/tech/:slug"element={ <TechnologyPage/>} />
+      </Routes>
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,23 @@
-function HomePage() {
+import { Link } from "react-router-dom";
+
+function HomePage(props) {
   return (
-    <div>
-      <h1>HomePage</h1>
-    </div>
+    <>
+      <div>
+        <h1>StackTracker: Discover Tech Stacks Used by Top Companies</h1>
+      </div>
+        {props.companies.map((aCompany) => {
+          return (
+            <>
+            <div key={aCompany.id}>
+              <Link to={`/aCompany/${aCompany.slug}`}></Link>
+              <p>{aCompany.name}</p>
+              <img width={50} src={aCompany.logo} alt="can't load company logo" />
+      </div>
+            </>
+          );
+        })}
+    </>
   );
 }
 
