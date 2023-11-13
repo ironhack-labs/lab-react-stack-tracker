6923fdccd57a8d37e47a299b2b3c41cb6aaaad8a
Vidisha Shah <shahvidisha1996@gmail.com>
2023-11-13 09:30:59 +0100

    Lab done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import "./App.css";
+import companiesObj from "./companies.json";
+import technologiesObj from "./technologies.json";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import { Route, Routes } from "react-router-dom";
+import CompanyPage from "./pages/CompanyPage";
+import TechnologyPage from "./pages/TechnologyPage";
 
 function App() {
+
+  const [companies, setCompanies] = useState(companiesObj);
+  const [technologies, setTechnologies] = useState(technologiesObj);
+
   return (
+
     <div className="App">
-      <h1>LAB | React Stack Tracker</h1>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<HomePage company = {companies}/>}/>
+        <Route path="/company/:companySlug" element={<CompanyPage company = {companies}/>}/>
+        <Route path="/tech" element={<TechnologyPage/>}/>
+      </Routes>
+      
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,25 @@
-function HomePage() {
+import { NavLink } from "react-router-dom";
+
+function HomePage({company}) {
+  
   return (
-    <div>
-      <h1>HomePage</h1>
+    
+    <div className="homepage">
+      <h1>StackTracker: Discover Tech Stacks Used by Top Companies</h1>
+      {
+        company.map((company)=>{
+          return (
+            <NavLink to = {`/company/${company.slug}`} key={company.id}>
+            <div className="card" style={{width: '18rem'}}>
+            <div className="card-body">
+                <h2 className="card-title">{company.name}</h2>
+            </div>
+              <img className="card-img-top" src={company.logo} alt="Card image cap" />
+            </div>
+            </NavLink>
+          );
+        })
+      }
     </div>
   );
 }
