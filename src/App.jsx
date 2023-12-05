be2ffa399fc2b35bfd3b534f569a2e684996c858
Brenda Silveira <brendasilveira.2010@hotmail.com>
2023-12-05 18:33:03 +0000

    lab finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import "./App.css";
+import companiesData from "./companies.json";
+import technologiesData from "./technologies.json";
+import { Routes, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import CompanyPage from "./pages/CompanyPage";
+import HomePage from "./pages/HomePage";
+import TechnologyPage from "./pages/TechnologyPage";
+import { useState } from "react";
 
 function App() {
+  const [companies, setCompanies] = useState(companiesData);
+  const [technologies, setTechnologies] = useState(technologiesData);
+
   return (
     <div className="App">
-      <h1>LAB | React Stack Tracker</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage companies={companies} />} />
+        <Route path="/company/:companySlug" element={<CompanyPage companies={companies} />} />
+        <Route path="/tech/:slug" element={<TechnologyPage technologies={technologies} />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,27 @@
-function HomePage() {
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function HomePage({ companies }) {
   return (
     <div>
-      <h1>HomePage</h1>
+      <h1 className="banner-hp">StackTracker: Discover Tech Stacks Used by Top Companies</h1>
+      {companies.map(company => {
+        return (
+          <div
+            key={company.id}
+            className='company'
+          >
+          <ul>
+            <Link className='companies' to={`/company/${company.slug}`}>
+              <>
+                <h3>{company.name}</h3>
+                <img className='logo' src={company.logo} alt="company-logo" />
+              </>
+            </Link>
+          </ul>
+          </div>
+        );
+      })}
     </div>
   );
 }
