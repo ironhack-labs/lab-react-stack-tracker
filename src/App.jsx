7c654644df41ff8cdbeae6852d2135ce999dbaa2
Ioannis Tourtouras <yiannistourtce@gmail.com>
2024-01-27 18:04:32 +0200

    Iteration 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import "./App.css";
+import Companies from './companies.json'
+import Technologies from './technologies.json'
+import {useState} from 'react'
+
 
 function App() {
+  const [companies, setCompanies] = useState(Companies)
+  const [technologies, setTechnologies] = useState(Technologies)
+
   return (
     <div className="App">
       <h1>LAB | React Stack Tracker</h1>
