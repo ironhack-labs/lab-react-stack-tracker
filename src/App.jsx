d6034f6d9df986d6ae22522e28bb748e14c11c51
jorge Centeno <eljorgeceneno@gmail.com>
2024-01-28 19:05:47 +0100

    first day

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+/* eslint-disable no-unused-vars */
 import "./App.css";
+import companiesJson from "./companies.json"
+import CompanyPage from "./pages/CompanyPage";
+import HomePage from "./pages/HomePage";
+import TechnologyPage from "./pages/TechnologyPage";
+import technologiesJson from "./technologies.json"
+import {Routes,Route} from "react-router-dom"
+import { useState } from "react";
 
 function App() {
+  const [companies,setCompanies] = useState(companiesJson)
   return (
     <div className="App">
       <h1>LAB | React Stack Tracker</h1>
+      <Routes>
+
+        <Route path="/" element={<HomePage companies={companies} setCompanies={setCompanies}></HomePage>}></Route>
+        
+        <Route path="/company/:companySlug" element={<CompanyPage></CompanyPage>}></Route>
+        <Route path="/tech/:slug" element={<TechnologyPage></TechnologyPage>}></Route>
+
+      </Routes>
     </div>
   );
 }
-
 export default App;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,18 @@
-function HomePage() {
+import {Link} from "react-router-dom"
+function HomePage(props) {
   return (
     <div>
-      <h1>HomePage</h1>
+      <h1>StackTracker: Discover Tech Stacks Used by Top Companies</h1>
+
+ {props.companies.map((eachCompany)=>{
+return(
+  <div>
+ <Link to={"/"}>{eachCompany.name}</Link>
+ <Link to={"/"}>{eachCompany.logo}</Link>
+ </div>
+)
+
+})}
     </div>
   );
 }
