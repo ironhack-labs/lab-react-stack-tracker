e3a9d355d85f4e9bd90039a51bdc0270445dfcef
Irene <amoreperdesi@gmail.com>
2024-01-30 00:48:17 +0100

    Iterations 1-5

--- src/pages/CompanyPage.jsx
+++ src/pages/CompanyPage.jsx
@@ -1,9 +1,31 @@
-function CompanyPage() {
-  return (
+import * as React from "react";
+import {useParams} from "react-router-dom";
+
+
+function CompanyPage(props) {
+
+  const {companyID}= useParams();
+  console.log(companyID)
+
+  let foundCompany = props.companies.find((company) => {
+    return company.id === companyID
+  })
+  
+
+return(
+<div>
+  <h2>Company profile</h2>
+  {!foundCompany && <h4>Not found</h4>}
+  {foundCompany && (
     <div>
-      <h1>CompanyPage</h1>
+    <img src={foundCompany.logo} alt={foundCompany.name + " logo"}></img>
+    <h3>{foundCompany.name}</h3>
+    <p>{foundCompany.description}</p>
     </div>
-  );
+  )}
+
+</div>
+)
 }
 
 export default CompanyPage;
