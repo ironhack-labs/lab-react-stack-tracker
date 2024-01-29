6d5b7d2662b48ec2547b93227a2984f5079a6627
Ioannis Tourtouras <yiannistourtce@gmail.com>
2024-01-29 14:20:32 +0200

    Iteration 5

--- src/pages/CompanyPage.jsx
+++ src/pages/CompanyPage.jsx
@@ -1,7 +1,30 @@
-function CompanyPage() {
+import React from "react";
+import {useParams} from "react-router-dom";
+import TechnologyPage from "./TechnologyPage";
+
+
+function CompanyPage(props) {
+  const { companySlug } = useParams();
+
+  let foundCompany = props.companies.find((company) => {
+    return company.slug === companySlug;
+  });
+
   return (
     <div>
-      <h1>Company Profile</h1>
+      <h2>Company Profile</h2>
+      <div>
+        {!foundCompany && <h3>No matching company</h3>}
+        {foundCompany && (
+          <div>
+            <h3>{foundCompany.name}</h3>
+            <p>{foundCompany.website}</p>
+            <p>{foundCompany.description}</p>
+            <img src={foundCompany.logo} alt='logo from found company' width="250"></img>        
+          <TechnologyPage foundCompany={foundCompany}></TechnologyPage>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- src/pages/TechnologyPage.jsx
+++ src/pages/TechnologyPage.jsx
@@ -1,7 +1,22 @@
-function TechnologyPage() {
+import {Link} from 'react-router-dom'
+import {v4 as randomId} from 'uuid'
+
+function TechnologyPage(props) {
+  
+
   return (
     <div>
-      <h1>Technology Details</h1>
+      <h2>Technology Details</h2>
+      {props.foundCompany.techStack.map((oneTechStack)=> {
+        return (
+          <div key={randomId()}>
+          <Link to={`company/tech/${oneTechStack.slug}`}>
+            <h4>{oneTechStack.name}</h4>
+            <img src={oneTechStack.image} alt='TechStack image' width="150" ></img>
+          </Link>
+          </div>
+        )
+      })}
     </div>
   );
 }
