b870c867452c68f90abceac720193c229097881b
Alastair Longmuir <alastairlinwood@gmail.com>
2024-02-07 15:24:51 +0100

    iteration 4 complete

--- src/pages/CompanyPage.jsx
+++ src/pages/CompanyPage.jsx
@@ -1,7 +1,16 @@
-function CompanyPage() {
+import {Link, useParams} from 'react-router-dom';
+
+function CompanyPage({companies}) {
+
+
+
+  let companySlug = useParams();
+
+  const companyProfile = companies.find(element => element.slug === companySlug);
+
   return (
     <div>
-      <h1>CompanyPage</h1>
+      <h1>{companySlug}</h1>
     </div>
   );
 }
