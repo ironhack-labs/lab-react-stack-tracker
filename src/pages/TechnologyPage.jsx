8e2795c4a3bc8940c1458060886b7eb25aaebbd8
Amparo Cabezuelo Sepulveda <amcasep@gmail.com>
2024-02-14 21:36:23 +0100

    lab solved except bonus

--- src/pages/TechnologyPage.jsx
+++ src/pages/TechnologyPage.jsx
@@ -1,7 +1,30 @@
-function TechnologyPage() {
+import { Link, useParams, useNavigate, useSearchParams  } from "react-router-dom";
+
+function TechnologyPage({technologies}) {
+
+  const {slug} = useParams()
+
+  const technologyDetails = technologies.find(technology => technology.slug === slug);
+  console.log(technologyDetails)
+
+  const [searchParams] = useSearchParams()
+  // const currentCompanySlug = searchParams.get("company");
+  const navigate = useNavigate(); 
+  
+
   return (
     <div>
-      <h1>TechnologyPage</h1>
+      <h1 className="TechnologyPage">Technology Details</h1>
+      <div className="flex">
+        <img src={technologyDetails.image} />
+        <div className="block-container">
+          <h2>{technologyDetails.name}</h2>
+          <p>About</p>
+          <p>{technologyDetails.description}</p>
+        </div>
+      </div>
+      {/* <Link to={`/company/${currentCompanySlug}`}><button>Back</button></Link> */}
+      <button onClick={() => navigate(-1)}>Back</button>
     </div>
   );
 }
