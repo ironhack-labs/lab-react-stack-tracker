674849f72e8e356fa551a442486d9666b6136c59
Aron <emmanuel.aron@gmail.com>
2024-02-24 15:32:33 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
 import "./App.css";
+import companies from './companies.json'
+import technologies from './technologies.json'
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CompanyPage from "./pages/CompanyPage"
+import TechnologyPage from "./pages/TechnologyPage"
+
 
 function App() {
+
+  const [companiesToDisplay, setCompaniesToDisplay] = useState(companies)
+  const [technologiesToDisplay, setTechnologiesToDisplay] = useState(technologies)
+
   return (
-    <div className="App">
-      <h1>LAB | React Stack Tracker</h1>
-    </div>
+    <>
+      <div className="App">
+      
+      </div>
+      <Routes>
+        <Route path="/" element={<HomePage companiesToDisplay={companiesToDisplay} />} />
+        <Route path="/company/:companySlug" element={<CompanyPage companiesToDisplay={companiesToDisplay} />} />
+        <Route path="/tech/:slug" element={<TechnologyPage technologiesToDisplay={technologiesToDisplay} />} />
+      </Routes>
+    </>
   );
 }
 
--- src/pages/CompanyPage.jsx
+++ src/pages/CompanyPage.jsx
@@ -1,7 +1,52 @@
-function CompanyPage() {
+import '../App.css'
+import { useParams } from 'react-router-dom';
+import { Link } from "react-router-dom";
+
+
+function CompanyPage(props) {
+  console.log(props)
+
+  const { companySlug } = useParams();
+  const companyDetails = props.companiesToDisplay.find(element => {
+    const normalizedSlug = element.slug.trim().toLowerCase();
+    const normalizedCompanySlug = companySlug.trim().toLowerCase();
+    return normalizedSlug == normalizedCompanySlug
+  })
+
+  if (!companyDetails) {
+    return <div>No company found (for this slug)</div>;
+  }
+
   return (
     <div>
-      <h1>CompanyPage</h1>
+      <h3>Company Profile</h3>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div className='logo-company'>
+          <div className='logo-around'>
+            <img src={companyDetails.logo} />
+          </div>
+
+        </div>
+        <div className='text-company' style={{ marginLeft: '170px' }}>
+          <h1>{companyDetails.name}</h1>
+          <h3>About</h3>
+          <div><b>{companyDetails.description}</b></div>
+        </div>
+      </div>
+
+      <div className='techStack'>
+        {/* Loop over the techStack array */}
+        {companyDetails.techStack.map(tech => (
+          <div className='logo-company space'>
+            <div key={tech.slug}>
+
+             <Link to={`/tech/${tech.slug}`}> <img src={tech.image} alt={tech.name} /> </Link>
+              <h4>{tech.name}</h4>
+            </div>
+          </div>
+        ))}
+      </div>
+
     </div>
   );
 }
