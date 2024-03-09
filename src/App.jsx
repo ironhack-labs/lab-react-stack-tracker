94653692b7b8fc8265d8e4d82d9cb957917341e6
guzegui <guzmanegui@gmail.com>
2024-03-09 15:48:46 +0100

    Done Iteration 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import companiesJson from "./companies.json";
-import technologiesJson from "./technologies.json";
+import companies from "./companies.json";
+import technologies from "./technologies.json";
 import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
@@ -12,13 +12,45 @@ function App() {
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage companies={companies} />} />
         <Route path="/company/:companySlug" element={<CompanyPage />} />
         <Route path="/tech/:slug" element={<TechnologyPage />} />
+
+
+
+
+
       </Routes>
     </div>
   );
 }
 
+
+/*
+
+
+
+        <Route
+          path="/projects/:projectId"
+          element={<ProjectDetailsPage projects={projects} />}
+        />
+
+
+
+Iteration 4 | Display Companies as a List
+In the HomePage component, render the list of companies.
+
+Each list item should be a React Router Link showing the company name and logo.
+
+The component should take 1 prop:
+
+companies: The array of companies coming from the App. 
+This is the data from companies.json that you stored in the state of App in Iteration 1.
+
+To allow users to navigate to a specific company's details page, embed the company's slug in the URL for each Link. When any of the company name on the HomePage are clicked, the company slug should show up in the URL, and the user should be navigated to the company details page (CompanyPage).
+
+
+
+*/
 export default App;
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,41 @@
-function HomePage() {
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
+function HomePage({ companies }) {
   return (
-    <div>
-      <h1>StackTracker: Discover Tech Stacks Used by Top Companies</h1>
+    <div className="company">
+      {companies.map((company) => (
+        <div key={company.id}>
+          <Link to={`/company/${company.name}`}>
+            <h2>{company.name}</h2>
+            <img className="company-img" src={company.logo} alt="logo" />
+          </Link>
+        </div>
+      ))}
     </div>
   );
 }
 
+// prop validation
+
+HomePage.propTypes = {
+  companies: PropTypes.array.isRequired,
+};
+
 export default HomePage;
+
+/*
+In the HomePage component, render the list of companies.
+
+Each list item should be a React Router Link showing the company name and logo.
+
+The component should take 1 prop:
+
+companies: The array of companies coming from the App. 
+This is the data from companies.json that you stored in the state of App in Iteration 1.
+
+To allow users to navigate to a specific company's details page, embed the company's slug in the URL for each Link. When any of the company name on the HomePage are clicked, the company slug should show up in the URL, and the user should be navigated to the company details page (CompanyPage).
+
+
+
+*/
