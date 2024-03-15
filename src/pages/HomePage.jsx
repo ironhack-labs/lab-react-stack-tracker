7ac51ae12b4f5f7e811dae0bb08fe9a0fbedc7cb
Sowjanya Kambhampati <sowjanyaakella87@gmail.com>
2024-03-15 22:42:31 +0100

    Stack Tracker all iterations

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,28 @@
-function HomePage() {
+import { Link } from "react-router-dom"
+
+function HomePage(props) {
   return (
-    <div>
-      <h1>HomePage</h1>
-    </div>
+    <>
+      <div className="HomePage">
+        <h1>StackTracker: Discover Tech Stacks Used by Top Companies</h1>
+      </div>
+      <div className="HomePage-CompagnieList">
+        {
+          props.companiesList.map((companyElement) => {
+            return(
+              <Link to={`/company/${companyElement.slug}`}>
+                <div className="HomePage-CompagnieBox" key={companyElement.id}>
+                  <h3>{companyElement.name}</h3>
+                  <div className="HomePage-CompagnieBoxImg">
+                    <img src={companyElement.logo} alt="CompagnieLogo" />
+                  </div>
+                </div>
+              </Link>
+            )
+          })
+        }
+      </div>
+    </>
   );
 }
 
