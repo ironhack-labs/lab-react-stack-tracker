b784a41bec640c96a600374e1920b9efb69301ad
Bettina <bettike18989@gmail.com>
2024-03-16 10:33:40 +0100

    LAB-REACT-STACK-TRACKER-FULL

--- src/pages/CompanyPage.jsx
+++ src/pages/CompanyPage.jsx
@@ -1,8 +1,45 @@
-function CompanyPage() {
+import { useParams, Link } from "react-router-dom";
+
+function CompanyPage({ companies }) {
+  const { companySlug } = useParams();
+  const company = companies.find(({ slug }) => companySlug === slug);
+
   return (
-    <div>
-      <h1>CompanyPage</h1>
-    </div>
+    <main>
+      <div className="conteiner">
+        <h1 className="title">Company Profile</h1>
+        <div className="profile-wrapper">
+          <div className="profile-img">
+            <img
+              width={200}
+              height={200}
+              src={company.logo}
+              alt={company.name}
+            />
+          </div>
+          <div className="profile-info">
+            <h2>{company.name}</h2>
+            <p>{company.website}</p>
+            <p>{company.description}</p>
+          </div>
+        </div>
+      </div>
+      <ul className="company-list">
+          {company.techStack.map((stack) => (
+            <li className="company-item" key={stack.name}>
+              <Link className="company-link" to={`/tech/${stack.slug}`}>
+                <img
+                  width={70}
+                  height={70}
+                  src={stack.image}
+                  alt={stack.name}
+                />
+                {stack.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+    </main>
   );
 }
 
--- src/pages/TechnologyPage.jsx
+++ src/pages/TechnologyPage.jsx
@@ -1,8 +1,41 @@
-function TechnologyPage() {
+import { useParams, useNavigate } from "react-router-dom";
+
+function TechnologyPage({ technologies }) {
+  const navigate = useNavigate();
+  const { slug } = useParams();
+  const technology = technologies.find((tech) => tech.slug === slug);
+
   return (
-    <div>
-      <h1>TechnologyPage</h1>
-    </div>
+    <main>
+      <div className="conteiner">
+        <h1 className="title">Technology Details</h1>
+        <div className="tech-wrapper">
+          <div className="tech">
+            <div className="profile-img">
+            <img
+              width={200}
+              height={200}
+              src={technology.image}
+              alt={technology.name}
+            />
+            </div>
+            <div className="profile-info">
+              <h2>{technology.name}</h2>
+              <p>{technology.description}</p>
+            </div>
+          </div>
+          <button
+          className="btn"
+            onClick={(event) => {
+              event.preventDefault();
+              navigate(-1);
+            }}
+          >
+            Back
+          </button>
+        </div>
+      </div>
+    </main>
   );
 }
 
