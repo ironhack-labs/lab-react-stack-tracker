ed0e7cef9bac08945fd330c22b60ed7763f8b7fd
Gabriela Silva <gvsde8@gmail.com>
2024-03-25 19:47:58 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import Companies from "./pages/CompanyPage";
+import Technologies from "./pages/TechnologyPage";
 
 function App() {
+ 
+
   return (
-    <div className="App">
-      <h1>LAB | React Stack Tracker</h1>
-    </div>
+    <div className="App relative z-20 pt-20">
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/company/:companySlug" element={<Companies />} />
+        <Route path="/tech" element={<Technologies />} />
+         
+        </Routes>
+      </div>
   );
 }
 
