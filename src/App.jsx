35166351630d29bfe9cadd869a8583e1a158cbd2
smnraphael <simonraphael@hotmail.fr>
2024-04-07 21:11:54 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import companiesData from "./companies.json";
+import technologiesData from "./technologies.json";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import CompanyPage from "./pages/CompanyPage";
+import TechnologyPage from "./pages/TechnologyPage";
 
 function App() {
+  const [companies, setCompanies] = useState(companiesData);
+  const [technologies, setTechnologies] = useState(technologiesData);
+
   return (
     <div className="App">
-      <h1>LAB | React Stack Tracker</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage companies={companies} />} />
+        <Route
+          path="/company/:companySlug"
+          element={<CompanyPage companies={companies} />}
+        />
+        <Route
+          path="/tech/:slug"
+          element={<TechnologyPage technologies={technologies} />}
+        />
+      </Routes>
     </div>
   );
 }
--- src/pages/TechnologyPage.jsx
+++ src/pages/TechnologyPage.jsx
@@ -1,7 +1,22 @@
-function TechnologyPage() {
+import { useParams } from "react-router-dom";
+
+function TechnologyPage({ technologies }) {
+  const { slug } = useParams();
+
+  const technology = technologies.find(
+    (technology) => technology.slug === slug
+  );
+
+  if (!technology) {
+    return <div>No technology</div>;
+  }
+
   return (
     <div>
-      <h1>TechnologyPage</h1>
+      <h1>Technology Details</h1>
+      <p>{technology.name}</p>
+      <img src={technology.image} alt={technology.slug} />
+      <p>{technology.description}</p>
     </div>
   );
 }
