945da8cd2c85cdc77b5ed3459803b3c1eda58992
Magdalena Draszczyk <magda.draszczyk@gmail.com>
2024-05-27 23:07:56 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import "./App.css";
+import companiesData from './companies.json';
+import technologiesData from './technologies.json';
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import CompanyPage from "./pages/CompanyPage";
+import TechnologyPage from "./pages/TechnologyPage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
+  const [companies, setCompanies] = useState(companiesData);
+  const [technologies, setTechnologies] = useState(technologiesData);
   return (
     <div className="App">
-      <h1>LAB | React Stack Tracker</h1>
+      <Navbar/>
+
+      <Routes>
+        <Route path='/' element={<HomePage allCompanies={companies}/>}/>
+        <Route path='/company/:companySlug' element={<CompanyPage allCompanies={companies}/>} />
+        <Route path='/tech/:technologySlug' element={<TechnologyPage allTechnologies={technologies}/>} />
+      </Routes>
+
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,24 @@
-function HomePage() {
+import { Link } from "react-router-dom";
+import { Card, Col } from "antd";
+
+
+function HomePage(props) {
   return (
-    <div>
-      <h1>HomePage</h1>
-    </div>
+    <Col>
+      <h1>StackTracker: Discover Tech Stacks Used by Top Companies</h1>
+      <div className="container">
+        {props.allCompanies.map(company => {
+          return (
+            <Link key={company.id} to={`/company/${company.slug}`} style={{ textDecoration: 'none' }}>
+            <Card className="company-container">
+              <h5>{company.name}</h5>
+              <img className="logo-img" src={company.logo} alt="" />
+            </Card>
+            </Link>
+          )
+        })}
+      </div>
+    </Col>
   );
 }
 
