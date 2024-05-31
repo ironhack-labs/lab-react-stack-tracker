2f181b2995b2edc09ab5f050871661d99826698f
bubu-destroi <zzzarolina@gmail.com>
2024-05-31 13:13:00 +0100

    solved lab

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,31 @@
-function HomePage() {
+import {Link} from 'react-router-dom'
+import { useParams } from 'react-router-dom';
+
+function HomePage(companies) {
+  
+
+
+
   return (
     <div>
-      <h1>HomePage</h1>
+      <h1>StackTracker: Discover Tech Stacks Used by Top Companies</h1>
+      {companies.map(company => {
+        return(
+          <div key= {company.id}>
+          <Link to={`/${company.slug}`}>
+          <h2>{company.name}</h2> 
+          <img src={company.logo} /> 
+          </Link>
+
+          </div>
+
+        )
+
+      })}
+
     </div>
   );
+  
 }
 
 export default HomePage;
--- src/pages/TechnologyPage.jsx
+++ src/pages/TechnologyPage.jsx
@@ -1,7 +1,13 @@
-function TechnologyPage() {
+import { useParams } from "react-router-dom";
+
+function TechnologyPage(technologies) {
+
+  const {slug}=useParams()
+
+  const foundTechSlug = technologies.find(tech => tech.slug === slug)
   return (
     <div>
-      <h1>TechnologyPage</h1>
+      <h1>Technology Details</h1>
     </div>
   );
 }
