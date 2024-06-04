bfd9fa9be77462d38c9ccfad6d158343e5ee6306
“Eva <evacmak@outlook.pt>
2024-06-04 20:11:47 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-import "./App.css";
+import {useState, useEffect} from "react";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CompanyPage from "./pages/CompanyPage";
+import TechnologyPage from "./pages/TechnologyPage";
+import companiesData from "./companies.json";
+import technologiesData from "./technologies.json";
+
 
 function App() {
+
+  const [companies, setCompanies] = useState([]);
+  const [technologies, setTechnologies] = useState([]);
+
+  useEffect(() => {
+
+    setCompanies(companiesData);
+    setTechnologies(technologiesData);
+  }, []);
+
   return (
     <div className="App">
       <h1>LAB | React Stack Tracker</h1>
+      <Switch>
+        <Route exact path="/" render={() => <HomePage companies={companies} technologies={technologies} />} />
+        <Route path="/company/:companySlug" component={CompanyPage} />
+        <Route path="/tech/:slug" component={TechnologyPage} />
+      </Switch>
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,27 @@
-function HomePage() {
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+function HomePage({ companies }) {
   return (
     <div>
       <h1>HomePage</h1>
+      <h2>List of Companies</h2>
+      <ul>
+        {companies.map((company) => (
+          <li key={company.id}>
+            <Link to={`/company/${company.slug}`}>
+              <img src={company.logo} alt={`${company.name} logo`} />
+              {company.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+HomePage.propTypes = {
+  companies: PropTypes.array.isRequired,
+};
 
 export default HomePage;
