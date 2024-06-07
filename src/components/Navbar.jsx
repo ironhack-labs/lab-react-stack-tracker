766a428f83781f2985dd00789dddca05a7f2f0ad
Daniela Pinheiro <daniela.plp@gmail.com>
2024-06-07 16:56:59 +0100

    labdone

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,18 @@
-function Navbar() {
-  return <nav>Navbar</nav>;
-}
+import { NavLink } from "react-router-dom";
 
+
+function Navbar() {
+  return (
+
+<nav className="navbar navbar-dark bg-primary mb-3">
+        <div className="container">
+          
+          StackTracker
+           
+        </div>
+      </nav>
+
+  )
+  
+}
 export default Navbar;
