4d2310054508ac4a76eec133ecf51e30781286d7
Ivan Ballester <irvan.baloncesto@gmail.com>
2024-07-11 20:53:58 +0200

    almost

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,31 @@
+import companies from "../companies.json";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 function HomePage() {
+  const [cursor, setCursor] = useState("default");
+
+  const handleMouseEnter = () => {
+    setCursor("pointer");
+  };
+
+  const handleMouseLeave = () => {
+    setCursor("default");
+  };
   return (
-    <div>
-      <h1>HomePage</h1>
+    <div className="homepage">
+      {companies.map((company) => (
+        <Link
+          key={company.id}
+          className="company cursor-change"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          style={{ cursor }}
+          to={`/company/${company.slug}`}
+        >
+          <p>{company.name}</p>
+          <img src={company.logo} alt="" width={"200px"} />
+        </Link>
+      ))}
     </div>
   );
 }
