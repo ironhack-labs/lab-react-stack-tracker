d8282ddff0b6483eea0892cfb3dec4cc0f0f1639
Sergio del Campo Puerta <sergio@sergioelmoreno.com>
2024-07-25 17:57:57 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import companies from "./companies.json"
+import technologies from "./technologies.json"
+import HomePage from "./pages/HomePage";
+import CompanyPage from "./pages/CompanyPage";
+import TechnologyPage from "./pages/TechnologyPage";
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React Stack Tracker</h1>
+      <Routes>
+        <Route path="/" element={<HomePage companies={companies} />} />
+        <Route path="/company/:companySlug" element={<CompanyPage companies={companies} />} />
+        <Route path="/tech/:slug" element={<TechnologyPage technologies={technologies} />} />
+      </Routes>
     </div>
   );
 }
