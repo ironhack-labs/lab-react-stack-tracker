aa3417c13a996b71335f3b1f45decb9b9e49dd4e
Ana Badolato <badolatoana@gmail.com>
2024-09-19 19:34:48 +0200

    Iteration 0

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h1>LAB | React Stack Tracker</h1>
+      
+      <Routes>
+
+      </Routes>
     </div>
+    
   );
 }
 
