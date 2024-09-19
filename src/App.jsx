4487d9e4847f9570a6bd810fc34f34671d7b2aad
Ana Badolato <badolatoana@gmail.com>
2024-09-19 19:43:59 +0200

    Iteration 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
 import { Routes, Route } from 'react-router-dom';
+import Companies from "./companies.json"
+import Technologies from "./Technologies.json"
+
+
 
 function App() {
+
+  const [companiesArr, setCompanies] = useState(Companies);
+  const [technologiesArr, setTechnologies] = useState(Technologies);
+
   return (
     <div className="App">
       <h1>LAB | React Stack Tracker</h1>
-      
+
+    <h2>Companies</h2>
+    <ul>
+      {companiesArr.map((company, index) => (
+        <li key={index}>
+          {company.name}
+        </li>
+        ))}
+    </ul>
+
+    <h2>Technologies</h2>
+    <ul>
+      {technologiesArr.map((tech, index) => (
+        <li key={index}>
+          {tech.name}
+        </li>
+      ))}
+    </ul>
+
       <Routes>
 
       </Routes>
