5f2eb2e52ae4afe4c8c993eae6aa4fd5ad80e950
Diego Lázaro <dlazarodecastro@gmail.com>
2024-09-22 20:02:03 +0200

    lab done

--- src/pages/CompanyPage.jsx
+++ src/pages/CompanyPage.jsx
@@ -1,8 +1,42 @@
-function CompanyPage() {
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+function CompanyPage({companies}) {
+
+  let {company} = useParams()
+
+  const companyData = companies.find((eachCompany) => {
+    return eachCompany.slug === company
+  })
+
   return (
+    <>
     <div>
-      <h1>CompanyPage</h1>
+      <h1>Company Profile</h1>
+      <div className="card">
+        <h3>{companyData.name}</h3>
+        <img className="logo" src={companyData.logo} alt="logo" />
+        <p>{companyData.description}</p>
+      </div>
     </div>
+
+    <div>
+      <ul className="lista-bottom">
+        {companyData.techStack.map((eachTech, index) => {
+          return (
+            <li key={index}>
+              <Link to={`/tech/${eachTech.slug}?company=${companyData.slug}`}>
+              <img src={eachTech.image} alt="logo" className="logo" />
+              <p>{eachTech.name}</p>
+              </Link>
+
+            </li>
+
+          )
+        })}
+      </ul>
+    </div>
+    </>
   );
 }
 
--- src/pages/TechnologyPage.jsx
+++ src/pages/TechnologyPage.jsx
@@ -1,7 +1,31 @@
-function TechnologyPage() {
+import { useParams } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
+
+function TechnologyPage({technologies}) {
+  let {tech} = useParams()
+  let [searchParams] = useSearchParams();
+
+  const companySlug = searchParams.get('company');
+
+  const techData = technologies.find((eachTech) => {
+    return eachTech.slug === tech
+  })
+  
   return (
+
     <div>
-      <h1>TechnologyPage</h1>
+      <h1>Technology Details</h1>
+
+      <div className="card">
+        <h3>{techData.name}</h3>
+        <img className="logo" src={techData.image} alt="logo" />
+        <p>{techData.description}</p>
+      </div>
+
+      <Link to={`/company/${companySlug}`}>
+        <button>Back</button>
+      </Link>
     </div>
   );
 }
