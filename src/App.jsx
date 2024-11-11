26b31d5599880d919ff66e70b2751d8a79aa789d
Sven Ole Spindler <s.spindler@gmx.de>
2024-11-11 17:50:58 +0100

    Import Data / created Folders, Pages and Components / Setup react router and create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import companiesData from "./companies.json";
+import Navbar from "./components/Navbar";
+import technologiesData from "./technologies.json";
+import { useState } from "react";
+import CompanyPage from "./pages/CompanyPage";
+import HomePage from "./pages/HomePage";
+import TechnologyPage from "./pages/TechnologyPage";
 
 function App() {
+  const [companies, setCompanies] = useState(companiesData);
+  const [technologies, setTechnologies] = useState(technologiesData);
+
   return (
     <div className="App">
-      <h1>LAB | React Stack Tracker</h1>
+      <Navbar />
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/tech/:slug" element={<TechnologyPage />} />
+          <Route path="/company/:companySlug" element={<CompanyPage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
