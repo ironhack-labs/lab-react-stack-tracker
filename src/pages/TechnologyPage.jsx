7074884104c286d0c83b6bf20ed485c0c011ab3e
RoniPG <ronigmn@gmail.com>
2024-11-29 13:42:16 +0100

    6.2 | Show Technology Details

--- src/pages/TechnologyPage.jsx
+++ src/pages/TechnologyPage.jsx
@@ -2,11 +2,16 @@ import { useParams } from "react-router-dom";
 
 function TechnologyPage({technologies}) {
   const {slug} = useParams();
-  console.log(slug);
+
+  const technology = technologies.find((tech) => tech.slug === slug)
+  console.log(technology);
   
   return (
     <div>
       <h1>Technology Details</h1>
+      {technology.name}
+      {technology.image}
+      {technology.description}
     </div>
   );
 }
