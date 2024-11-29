0d38750f85bfcbd5e231ec02506812306b9e7275
RoniPG <ronigmn@gmail.com>
2024-11-29 11:21:49 +0100

    Iteration 1 | Import Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import "./App.css";
+import companiesData  from "./companies.json";
+import technologiesData  from "./technologies.json";
+
 
 function App() {
+  const [companies, setCompanies] = useState(companiesData);
+  const [technologies, setTechnologies] = useState(technologiesData);
+
   return (
     <div className="App">
       <h1>LAB | React Stack Tracker</h1>
