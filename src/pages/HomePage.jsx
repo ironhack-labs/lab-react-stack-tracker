481ab3bd1084eabd67b7d76a1abf1c29712b4f10
RoniPG <ronigmn@gmail.com>
2024-11-29 12:07:24 +0100

    Iteration 4 | Display Companies as a List

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,14 @@
-function HomePage() {
+import { Link } from "react-router-dom";
+
+function HomePage({companies}) {
   return (
     <div>
       <h1>StackTracker: Discover Tech Stacks Used by Top Companies</h1>
+      {companies.map( company => (
+        <Link to={`/company/${company.slug}`}>{company.name}
+        <img src={company.logo} alt={company.name}  />
+        </Link>
+      ))}
     </div>
   );
 }
