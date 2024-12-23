c365829ca220338f441bbbbb906b0e823ec4eb07
Victor Hugo Guardiola <victorhugo.pessoal.guardiola@gmail.com>
2024-12-23 18:05:39 +0100

    iteration 1 | Import Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import "./App.css";
+import CompaniesData from "./companies.json"
+import TechnologiesData from "./technologies.json"
+
 
 function App() {
+
+  const [companies, setCompanies] = useState(CompaniesData);
+  // console.log(companies);
+  const [technologies, setTechnologies] = useState(TechnologiesData);
+  // console.log(technologies);
+
   return (
     <div className="App">
       <h1>LAB | React Stack Tracker</h1>
