c12829b0f1bc1c97c196d6e623fe9f0af4a2058e
Victor Hugo Guardiola <victorhugo.pessoal.guardiola@gmail.com>
2024-12-23 18:18:46 +0100

    iteration 3 | Setup React Router and Create Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from "react";
 import "./App.css";
-import CompaniesData from "./companies.json"
-import TechnologiesData from "./technologies.json"
+import CompaniesData from "./companies.json";
+import TechnologiesData from "./technologies.json";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import CompanyPage from "./pages/CompanyPage";
 import TechnologyPage from "./pages/TechnologyPage";
-
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-
   const [companies, setCompanies] = useState(CompaniesData);
   // console.log(companies);
   const [technologies, setTechnologies] = useState(TechnologiesData);
@@ -18,9 +17,11 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <HomePage />
-      <CompanyPage />
-      <TechnologyPage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/company/:companySlug" element={<CompanyPage />} />
+        <Route path="/tech/:slug" element={<TechnologyPage />} />
+      </Routes>
     </div>
   );
 }
