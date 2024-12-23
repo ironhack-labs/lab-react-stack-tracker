9bc112b6209c7cc3acd053341578ac8796b2982c
Victor Hugo Guardiola <victorhugo.pessoal.guardiola@gmail.com>
2024-12-23 21:29:46 +0100

    Bonus: iteration 8 | Styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
-  return <nav>StackTracker</nav>;
+  return (
+    <nav>
+      <Link to="/"><p>StackTracker</p></Link>
+    </nav>
+  );
 }
 
 export default Navbar;
