ae247b4c236ff480d163788338b2b4abc2b58df5
MMARTID <miguelmartindelgado1998@gmail.com>
2025-01-04 20:20:58 +0100

    set Routes & render json data-mstyled list on Home component, routes on root, dynamic path on companies render

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,23 @@ import Navbar from "./components/Navbar";
 import CompanyPage from "./pages/CompanyPage";
 import HomePage from "./pages/HomePage";
 import TechnologyPage from "./pages/TechnologyPage";
+import { Route, Routes } from "react-router-dom";
 
 
 function App() {
-const [companies, setCompanies] = useState('')
-const [technologies, setTechnologies] = useState('')
+const [companies, setCompanies] = useState(companiesData)
+const [technologies, setTechnologies] = useState(technologiesData)
 
   return (
     <div className="App">
       <Navbar/>
+      <Routes>
+      <Route path="/" element={<HomePage companies={companies} />}/>
+      <Route path="/company/:companySlug" element={<CompanyPage companies={companies} />} />
+      <Route path="/tech/:slug" element={<TechnologyPage companies={companies}/>}/>
+
+        
+      </Routes>
     </div>
   );
 }
